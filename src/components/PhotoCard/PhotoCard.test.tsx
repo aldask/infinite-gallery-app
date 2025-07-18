6a75e3d39ecad5b92fb1bff367800be424e74b53
Aldas Kasputis <80693733+aldask@users.noreply.github.com>
2025-07-19 00:25:00 +0300

    Improved test of PhotoCard

--- src/components/PhotoCard/PhotoCard.test.tsx
+++ src/components/PhotoCard/PhotoCard.test.tsx
@@ -2,3 +2,46 @@ import { render, screen } from "@testing-library/react";
 import PhotoCard from "./PhotoCard";
 import { describe, it, expect, vi } from "vitest";
 import "@testing-library/jest-dom/vitest";
+
+describe("PhotoCard rendering", () => {
+  vi.mock("..ProgressiveImage/ProgressiveImage", () => ({
+    default: ({
+      lowResSrc,
+      highResSrc,
+      alt,
+    }: {
+      lowResSrc: string;
+      highResSrc: string;
+      alt: string;
+    }) => (
+      <>
+        <img src={lowResSrc} alt={alt} />
+        <img src={highResSrc} alt={alt} />
+      </>
+    ),
+  }));
+
+  it("It should render the PhotoCard component", () => {
+    const mockPic = {
+      id: 0,
+      photographer: "mock photographer",
+      alt: "mock pic txt",
+      src: {
+        original: "mockOriginalImg",
+        portrait: "mockPortraitImg",
+        medium: "mockMediumImg",
+        small: "mockSmallImg",
+      },
+    };
+    render(
+      <PhotoCard photo={mockPic} isFavorite={true} onFavToggle={vi.fn()} />
+    );
+
+    expect(screen.getByText("mock pic txt")).toBeInTheDocument();
+    expect(screen.getByText("mock photographer")).toBeInTheDocument();
+    expect(
+      screen.getByRole("button", { name: "Unfavourite" })
+    ).toBeInTheDocument();
+    expect(screen.getAllByRole("img").length).toBeGreaterThan(0);
+  });
+});
